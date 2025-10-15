db4e35eebe3d9e0e0f7b800628b83b684b655166
Teodora <tdichim@gmail.com>
2025-10-15 16:44:43 -0400

    Update c6.lean

--- c6.lean
+++ c6.lean
@@ -1,3 +1,4 @@
+/- Copyright (c) Heather Macbeth, 2022.  All rights reserved. -/
 import Mathlib.Data.Nat.Basic
 import Library.Basic
 
@@ -5,23 +6,32 @@ math2001_init
 
 -- If c | a - b and c | b then c | a.
 lemma dvd_multiple_minus_multiple {a b c : ℤ} (h1 : c ∣ a - b) (h2 : c ∣ b) : c ∣ a := by
-  obtain⟨x,hx⟩ := h1
-  obtain⟨y,hy⟩ := h2
+  obtain ⟨x, hx⟩ := h1
+  obtain ⟨y, hy⟩ := h2
   use x + y
   calc
     a = a - b + b := by ring
-    _ = c * x + c * y := by rw[hx,hy]
+    _ = c * x + c * y := by rw [hx, hy]
     _ = c * (x + y) := by ring
 
 /-- If 3 ∤ n, then 13 ∣ (3 ^ (2 * n) + 3 ^ n + 1) -/
 
 theorem div_by_thirteen (n : ℕ) (h : ¬ (3 : ℤ) ∣ n) :
-  13 ∣ (3^(2*n) + 3^n + 1) := by
-  have h1 : ∀b, 3 ^ (3 * b) ≡ 1 [ZMOD 13] := by
-    sorry
+  13 ∣ (3 ^ (2 * n) + 3 ^ n + 1) := by 
+  have h1 : ∀ b, 3 ^ (3 * b) ≡ 1 [ZMOD 13] := by
+    intro b
+    have base : 3 ^ 3 ≡ 1 [ZMOD 13] := by
+      calc
+        3 ^ 3 = 27 := by ring
+        _ = 26 + 1 := by ring
+        _ ≡ 1 [ZMOD 13] := by use 2
+      calc
+        3 ^ (3 * b) = (3 ^ 3) ^ b := by rw [pow_mul]
+        _ ≡ 1 ^ b [ZMOD 13] := by rw [base]
+        _ ≡ 1 [ZMOD 13] := by ring
   have h2 : ∃k : ℕ, n = 3 * k + 1 ∨ n = 3 * k + 2 := by
     sorry
-  obtain⟨k,hk⟩ := h2
+  obtain ⟨k, hk⟩ := h2
   obtain h3 | h3 := hk
   . have h4 : 3 ^ (2 * 3 * k) * 3 ^ 2 ≡ 3 ^ 2 [ZMOD 13] := by
       sorry
@@ -30,6 +40,10 @@ theorem div_by_thirteen (n : ℕ) (h : ¬ (3 : ℤ) ∣ n) :
     sorry
   . have h4 : 3 ^ (2 * 3 * k) * 3 ^ 2 ≡ 3 ^ 2 [ZMOD 13] := by
       sorry
+    have h5 : 3 ^ (3 * k) * 3 ≡ 3 [ZMOD 13] := by
+      sorry
+    sorry
+
     have h5 : 3 ^ (3 * k) * 3 ≡ 3 [ZMOD 13] := by
       sorry
     sorry
