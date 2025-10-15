513a57e099c5a6cbcd665e1caecfc7b95b2b8fae
Thomas Salter <tommysalterburrows@gmail.com>
2025-10-15 15:25:26 -0400

    Made skeleton

--- c6.lean
+++ c6.lean
@@ -16,5 +16,20 @@ lemma dvd_multiple_minus_multiple {a b c : ℤ} (h1 : c ∣ a - b) (h2 : c ∣ b
 /-- If 3 ∤ n, then 13 ∣ (3^(2*n) + 3^n + 1) -/
 
 theorem div_by_thirteen (n : ℕ) (h : ¬ (3 : ℤ) ∣ n) :
-  13 ∣ (3^(2*n) + 3^n + 1) :=
-sorry
+  13 ∣ (3^(2*n) + 3^n + 1) := by
+  have h1 : ∀b, 3 ^ (3 * b) ≡ 1 [ZMOD 13] := by
+    sorry
+  have h2 : ∃k : ℕ, n = 3 * k + 1 ∨ n = 3 * k + 2 := by
+    sorry
+  obtain⟨k,hk⟩ := h2
+  obtain h3 | h3 := hk
+  . have h4 : 3 ^ (2 * 3 * k) * 3 ^ 2 ≡ 3 ^ 2 [ZMOD 13] := by
+      sorry
+    have h5 : 3 ^ (3 * k) * 3 ≡ 3 [ZMOD 13] := by
+      sorry
+    sorry
+  . have h4 : 3 ^ (2 * 3 * k) * 3 ^ 2 ≡ 3 ^ 2 [ZMOD 13] := by
+      sorry
+    have h5 : 3 ^ (3 * k) * 3 ≡ 3 [ZMOD 13] := by
+      sorry
+    sorry
