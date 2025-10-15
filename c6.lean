8b72607b4eff82eebd2f241559d816fcf5729233
Teodora <tdichim@gmail.com>
2025-10-15 16:07:55 -0400

    Update c6.lean

--- c6.lean
+++ c6.lean
@@ -13,7 +13,7 @@ lemma dvd_multiple_minus_multiple {a b c : ℤ} (h1 : c ∣ a - b) (h2 : c ∣ b
     _ = c * x + c * y := by rw[hx,hy]
     _ = c * (x + y) := by ring
 
-/-- If 3 ∤ n, then 13 ∣ (3^(2*n) + 3^n + 1) -/
+/-- If 3 ∤ n, then 13 ∣ (3 ^ (2 * n) + 3 ^ n + 1) -/
 
 theorem div_by_thirteen (n : ℕ) (h : ¬ (3 : ℤ) ∣ n) :
   13 ∣ (3^(2*n) + 3^n + 1) := by
